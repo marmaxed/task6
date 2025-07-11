78fdc7f441fdde60ee217ca6301e8c42f59a8d05
marmaxed <89139747+marmaxed@users.noreply.github.com>
2025-07-11 19:29:00 +0300

    Add files via upload

---
+++ components/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import WorldClockPage from "./pages/WorldClockPage";
+
+const App = () => {
+  return (
+    <div>
+       <WorldClockPage />
+    </div>
+  );
+};
+
+export default App;
